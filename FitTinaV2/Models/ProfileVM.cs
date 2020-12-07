bacf247128f74253791e9e952d860241e4b54ce8
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-07 12:20:01 -0800

    Added properties and height helper methods

--- FitTinaV2/Models/ProfileVM.cs
+++ FitTinaV2/Models/ProfileVM.cs
@@ -3,8 +3,29 @@ namespace FitTinaV2.Models
 {
     public class ProfileVM
     {
-        public ProfileVM()
+        // needs from user:
+        // name, age, feet, inches, weight, fitnessgoal, equipmentpref
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Feet { get; set; }
+        public int Inches { get; set; }
+        public int Weight { get; set; }
+        public string FitnessGoal { get; set; }
+        public string EquipmentPref { get; set; }
+
+        public int TotalHeightToFeet(int height)
         {
+            int feet = height / 12;
+
+            return feet;
+        }
+
+        public int TotalHeightToRemainingInches(int height)
+        {
+            int inches = height % 12;
+
+            return inches;
         }
     }
 }

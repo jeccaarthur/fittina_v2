e8794162ecb2c30dd7c9f3a941782f3a3acc6ef7
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-07 13:19:59 -0800

    Added height helper methods

--- FitTinaV2/Models/User.cs
+++ FitTinaV2/Models/User.cs
@@ -20,5 +20,26 @@ namespace FitTinaV2.Models
         public int Weight { get; set; }
         public string FitnessGoal { get; set; }
         public string EquipmentPref { get; set; }
+
+        public int FeetInchesToTotalHeight(int feet, int inches)
+        {
+            int totalInches = (feet * 12) + inches;
+
+            return totalInches;
+        }
+
+        public int TotalHeightToFeet(int height)
+        {
+            int feet = height / 12;
+
+            return feet;
+        }
+
+        public int TotalHeightToRemainingInches(int height)
+        {
+            int inches = height % 12;
+
+            return inches;
+        }
     }
 }

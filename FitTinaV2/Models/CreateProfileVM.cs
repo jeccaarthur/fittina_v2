447972477acb2c6c3e29f689f12e83ccf5f28ac8
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-07 12:14:44 -0800

    Added methods for height conversion

--- FitTinaV2/Models/CreateProfileVM.cs
+++ FitTinaV2/Models/CreateProfileVM.cs
@@ -4,13 +4,7 @@ namespace FitTinaV2.Models
     public class CreateProfileVM
     {
         // needs from user:
-        // name
-        // age
-        // feet
-        // inches
-        // weight
-        // fitnessgoal
-        // equipmentpref
+        // name, age, feet, inches, weight, fitnessgoal, equipmentpref
 
         public string Name { get; set; }
         public int Age { get; set; }
@@ -20,5 +14,18 @@ namespace FitTinaV2.Models
         public string FitnessGoal { get; set; }
         public string EquipmentPref { get; set; }
 
+        public int TotalHeightToFeet(int height)
+        {
+            int feet = height / 12;
+
+            return feet;
+        }
+
+        public int TotalHeightToRemainingInches(int height)
+        {
+            int inches = height % 12;
+
+            return inches;
+        }
     }
 }

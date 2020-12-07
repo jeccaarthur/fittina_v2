67e14de15141c690f64ded575553ea00a20ab9c7
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-07 12:06:11 -0800

    Added properties

--- FitTinaV2/Models/CreateProfileVM.cs
+++ FitTinaV2/Models/CreateProfileVM.cs
@@ -3,6 +3,22 @@ namespace FitTinaV2.Models
 {
     public class CreateProfileVM
     {
-        
+        // needs from user:
+        // name
+        // age
+        // feet
+        // inches
+        // weight
+        // fitnessgoal
+        // equipmentpref
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Feet { get; set; }
+        public int Inches { get; set; }
+        public int Weight { get; set; }
+        public string FitnessGoal { get; set; }
+        public string EquipmentPref { get; set; }
+
     }
 }

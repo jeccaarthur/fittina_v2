e376d6780feb14e185100e135d4345d541729cbe
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-06 22:05:16 -0800

    Added config for db connection

--- FitTinaV2/Startup.cs
+++ FitTinaV2/Startup.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FitTinaV2.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Runtime.InteropServices;
 
 namespace FitTinaV2
 {
@@ -24,6 +27,22 @@ namespace FitTinaV2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages()
+            .AddRazorRuntimeCompilation();
+
+            // add if statement to support azure db
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                services.AddDbContext<FitTinaDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:AzureSQLServerConnection"]));
+            }
+            else
+            {
+                services.AddDbContext<FitTinaDbContext>(options => options.UseSqlite(Configuration["ConnectionStrings:SQLiteConnection"]));
+            }
+
+            // injects repository into any controller that has it specified in its constructor
+            // services.AddTransient<IUserRepository, UserRepository>();
+            // services.AddTransient<IWorkoutRepository, WorkoutRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

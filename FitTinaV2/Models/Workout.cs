2af630b098f570cd513eeb6f5b66754b840145d1
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-07 16:59:56 -0800

    Updated properties (UserName and Effort)

--- FitTinaV2/Models/Workout.cs
+++ FitTinaV2/Models/Workout.cs
@@ -20,8 +20,58 @@ namespace FitTinaV2.Models
         public Exercise Exercise5 { get; set; }
         public Exercise Exercise6 { get; set; }
 
+        [NotMapped]
+        public string Effort { get; set; }
+
         [ForeignKey("User")]
         public int UserID { get; set; }
         public User User { get; set; }
+
+        [NotMapped]
+        public string UserName { get; set; }
+
+
+        // convert effort to sets
+        public int GetSets(string effort)
+        {
+            int sets = 1;
+
+            if (effort == "easy")
+            {
+                sets = 2;
+            }
+            else if (effort == "medium")
+            {
+                sets = 3;
+            }
+            else if (effort == "challenging")
+            {
+                sets = 4;
+            }
+
+            return sets;
+        }
+
+
+        // convert effort to reps
+        public int GetReps(string effort)
+        {
+            int reps = 8;
+
+            if (effort == "easy")
+            {
+                reps = 10;
+            }
+            else if (effort == "medium")
+            {
+                reps = 12;
+            }
+            else if (effort == "challenging")
+            {
+                reps = 15;
+            }
+
+            return reps;
+        }
     }
 }

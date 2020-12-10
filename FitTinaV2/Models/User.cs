2cb5bea0f092822ee0e053215804e7b3bc3122ed
Jecca Arthur <arthurj@my.lanecc.edu>
2020-12-10 13:58:56 -0800

    Removed height conversion methods - will be storing feet and inches in db as well

--- FitTinaV2/Models/User.cs
+++ FitTinaV2/Models/User.cs
@@ -23,21 +23,5 @@ namespace FitTinaV2.Models
 
             return totalInches;
         }
-
-        /*
-        public int TotalHeightToFeet(int height)
-        {
-            int feet = height / 12;
-
-            return feet;
-        }
-
-        public int .TotalHeightToRemainingInches(int height)
-        {
-            int inches = height % 12;
-
-            return inches;
-        }
-        */
     }
 }
